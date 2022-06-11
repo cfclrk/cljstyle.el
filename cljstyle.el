9ec18549e3db3de81c43b305aedfc6da83669a5d
Chris Clark <cfclrk@gmail.com>
2022-06-11 09:30:09 -0400

    Add a customize group

--- cljstyle.el
+++ cljstyle.el
@@ -1,4 +1,4 @@
-;;; cljstyle.el --- Reformat Clojure code with cljstyle -*- lexical-binding: t; -*-
+;;; cljstyle.el --- Format Clojure code with cljstyle -*- lexical-binding: t; -*-
 
 ;; Version: 0
 ;; Package-Requires: ((reformatter "0"))
@@ -9,9 +9,27 @@
 
 (require 'reformatter)
 
+(defgroup cljstyle nil
+  "Format clojure code using cljstyle."
+  :group 'languages
+  :prefix "cljstyle-")
+
+(defcustom cljstyle-command "cljstyle"
+  "Command to invoke cljstyle."
+  :type 'file
+  :group 'cljstyle
+  :safe 'stringp)
+
+(defcustom cljstyle-arguments '("pipe")
+  "Aruguments to pass to cljstyle."
+  :type 'file
+  :group 'cljstyle
+  :safe 'listp)
+
 (reformatter-define cljstyle-format
-  :program "cljstyle"
-  :args '("pipe")
+  :program cljstyle-command
+  :args cljstyle-arguments
+  :group 'cljstyle
   :lighter " cljstyle")
 
 (provide 'cljstyle)
